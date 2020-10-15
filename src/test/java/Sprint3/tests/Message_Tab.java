897ce3226dd34cdba54a9312b558148aa5ccc0ce
karaduran <17karaduran@gmail.com>
2020-10-15 17:17:38 -0400

    update

--- src/test/java/Sprint3/tests/Message_Tab.java
+++ src/test/java/Sprint3/tests/Message_Tab.java
@@ -1,13 +1,17 @@
 package Sprint3.tests;
 
+import Sprint3.util.BrowserUtil;
 import Sprint3.util.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.List;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 public class Message_Tab {
@@ -40,6 +44,199 @@ public class Message_Tab {
 
         //new update
     }
+    //==============================================================================================
+
+    @Test
+    public void TestCase_4_to_6(){
+
+
+        //4-Then user click Upload files and images link √
+        //Test Case 4-6
+
+
+
+        driver.findElement(By.xpath("//span[.='Message']")).click();
+        driver.findElement(By.xpath("//span[@id='bx-b-uploadfile-blogPostForm']")).click();
+        BrowserUtil.Wait(2);
+        driver.findElement(By.xpath("//input[@name='bxu_files[]']")).sendKeys("/Users/karaduran/Desktop/CRM/Test.pdf ");
+        BrowserUtil.Wait(2);
+        boolean file_displayed = driver.findElement(By.xpath("//span[@class='files-text']")).isDisplayed();
+        Assert.assertTrue(file_displayed,"File is not uploaded");
+
+
+
+
+    }
+
+
+
+
+
+
+
+    @Test
+    public void TestCase7_to_9() {
+        /*
+        7-Then user click Download from external drive link
+        8-Then user selects available document
+        9-Then user clicks Select document button
+         */
+
+
+
+
+
+        driver.findElement(By.xpath("//span[@class='feed-add-post-micro-title']")).click();
+        BrowserUtil.Wait(1);
+        //3-And user click Upload files icon √
+        driver.findElement(By.id("bx-b-uploadfile-blogPostForm")).click();
+        BrowserUtil.Wait(1);
+
+
+        //7-Then user click Download from external drive link
+        driver.findElement(By.xpath("(//span[.='Download from external drive'])[1]")).click();
+        BrowserUtil.Wait(2);
+        //external links
+        List<WebElement> listOfExternalLinks = driver.findElements(By.xpath("//div[@class='bx-file-dialog-tab-item']"));
+        for (WebElement eachLink : listOfExternalLinks) {
+            System.out.println(eachLink.getText());
+            eachLink.click();
+            BrowserUtil.Wait(1);
+
+        }
+
+        //<span class="popup-window-button popup-window-button-link popup-window-button-link-cancel" id="">Cancel</span>
+        List<WebElement> listOfExternalPopUps = driver.findElements(By.xpath("//span[@class='popup-window-button popup-window-button-link popup-window-button-link-cancel']"));
+        for (WebElement each : listOfExternalPopUps) {
+            System.out.println(each.getText());
+        }
+
+
+        driver.findElement(By.xpath("//span[@class='popup-window-button popup-window-button-link popup-window-button-link-cancel']")).click();
+        BrowserUtil.Wait(2);
+        Assert.fail("Download from external drive module has not been developed");
+
+
+
+
+
+    }
+
+
+    @Test
+    public void SelectDocFromBitrix24_TestCase10_12(){
+
+        driver.findElement(By.xpath("//span[@class='feed-add-post-micro-title']")).click();
+        BrowserUtil.Wait(1);
+        //3-And user click Upload files icon √
+        driver.findElement(By.id("bx-b-uploadfile-blogPostForm")).click();
+        BrowserUtil.Wait(1);
+
+        /*
+        10-Then user clicks Select document from Bitrix24 link
+        11-Then user select available files
+        12-Then user click select document button
+
+         */
+
+        //10-Then user clicks Select document from Bitrix24 link
+        driver.findElement(By.xpath("//span[@class='wd-fa-add-file-light-title-text diskuf-selector-link'][1]")).click();
+
+        //11-Then user select available files
+        List<WebElement> listOfDocs = driver.findElements(By.xpath("//a[@class='bx-file-dialog-content-link bx-file-dialog-icon bx-file-dialog-icon-file']"));
+        for (WebElement eachDoc:listOfDocs){
+            System.out.println(eachDoc.getText());
+            eachDoc.click();
+        }
+        //12-Then user click select document button
+        driver.findElement(By.xpath("//span[@class='popup-window-button popup-window-button-accept']")).click();
+
+
+        //Verification for each selected doc
+        List<WebElement> displayedDocs = driver.findElements(By.xpath("//span[@class='files-text']"));
+        for (WebElement eachDoc : displayedDocs){
+            boolean correction = eachDoc.isDisplayed();
+            Assert.assertTrue(correction,"Selected docs did not displayed, Verification Failed");
+        }
+
+
+    }
+
+    @Test
+    public void DesktopApp_TestCase_13_to_15(){
+
+
+        driver.findElement(By.xpath("//span[@class='feed-add-post-micro-title']")).click();
+        BrowserUtil.Wait(1);
+        //3-And user click Upload files icon √
+        driver.findElement(By.id("bx-b-uploadfile-blogPostForm")).click();
+        BrowserUtil.Wait(1);
+
+        /*
+        13-Then user clicks Create using Desktop applications link
+        14-Then user selects an option
+        15-Then user verify selected files uploaded successfully
+         */
+        driver.findElement(By.xpath("//span[@onclick='DiskOpenMenuCreateService(this); return false;'][1]")).click();
+
+        List<WebElement> listOfCreatingUsing = driver.findElements(By.xpath("//span[@class='menu-popup-item-text']"));
+        for (WebElement each: listOfCreatingUsing){
+            System.out.println(each.getText());
+        }
+
+
+
+        //driver.findElement(By.xpath("(//span[@class='wd-fa-add-file-light-title-text'])[3]")).click();
+
+        //BrowserUtils.wait(2);
+
+        driver.findElement(By.xpath("(//span[@class='menu-popup-item-text'])[1]")).click();
+        BrowserUtil.Wait(2);
+
+        driver.findElement(By.xpath("//span[@class='popup-window-button']")).click();
+        BrowserUtil.Wait(2);
+
+        driver.findElement(By.xpath("(//a[@class='wd-fa-add-file-editor-link-block'])[1]")).click();
+
+        String mainHandle = driver.getWindowHandle();
+        System.out.println("mainHandle = " + mainHandle);
+
+
+        //set allows only unique values
+
+        Set<String> windowHandles = driver.getWindowHandles();
+
+        for (String each:windowHandles){
+            driver.switchTo().window(each);
+            System.out.println("Current title = "+driver.getTitle());
+            BrowserUtil.Wait(1);
+
+        }
+        System.out.println(driver.findElement(By.id("error-text")).getText());
+        BrowserUtil.Wait(1);
+
+
+
+        driver.close();
+        driver.switchTo().window(mainHandle);
+        Assert.fail("User cannot selects an option due to incomplete configuration! ");
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+    //===========================================================================================
+
+
+
 
     @AfterMethod
     public void tearDown(){
